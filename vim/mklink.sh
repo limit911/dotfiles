552d8b846744ccdd6bdb64cca7c0c82d8a9a8d7f
Author LiMit <limit911@gmail.com>
2017-02-18 16:30:08 +0800

    First Commit

---
+++ vim/mklink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BACK_TIME=$(date +"%Y%m%d")
+
+if [ -L ~/.vim ]; then
+    mv ~/.vim ~/.vim_${BACK_TIME}_bak
+fi
+
+if [ -L ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc_${BACK_TIME}_bak
+fi
+
+ln -s $SELF_DIR/vimrcs/vimrc ~/.vimrc
+ln -s $SELF_DIR/vimfiles ~/.vim
