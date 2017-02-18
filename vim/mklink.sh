b8dc3a2b539a67585da4b04ee2fb564f1879da15
LiMit <limit911@gmail.com>
2017-02-18 16:51:23 +0800

    Check if .vimrc is link file.

--- vim/mklink.sh
+++ vim/mklink.sh
@@ -3,11 +3,19 @@
 SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACK_TIME=$(date +"%Y%m%d")
 
-if [ -L ~/.vim ]; then
+if [ -L ~/.vim ]
+then
+    rm ~/.vim
+elif [ -d ~/.vim ]
+then
     mv ~/.vim ~/.vim_${BACK_TIME}_bak
 fi
 
-if [ -L ~/.vimrc ]; then
+if [ -L ~/.vimrc ]
+then
+    rm ~/.vimrc
+elif [ -f ~/.vimrc ]
+then
     mv ~/.vimrc ~/.vimrc_${BACK_TIME}_bak
 fi
 
